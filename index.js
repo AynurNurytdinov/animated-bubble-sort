c337d98795d7fedf996ba02a45da7976901022e3
unknown <a.nurytdinov@gmail.com>
2019-07-11 16:30:45 +0300

    draw container

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+// генерация массива
+// проверка на целые числа
+// начальная форма и инструкции
+
 class BubbleSort {
-   constructor() {}
+   container = document.getElementById('container');
+   arr = [52, 116, 73, 15, 122, 132, 88, 43, 97];
+
+   max = Math.max(...this.arr);
+
+   console() {
+      console.log(this.max);
+   }
+
+   fillContainer() {
+      for (let i = 0; i < this.arr.length; i++) {
+         let item = document.createElement('div');
+         item.className = 'item';
+         item.innerHTML = `<span>${this.arr[i]}</span>`;
+         let heightItem = Math.round(this.arr[i] * 1000 / this.max) / 10;
+         item.style = `height: ${heightItem}%; margin-top: ${100 - heightItem}%`;
+         this.container.appendChild(item);
+      }
+   }
+
+   sort() {
+
+   }
 }
+
+BubbleSort = new BubbleSort();
+BubbleSort.fillContainer();
